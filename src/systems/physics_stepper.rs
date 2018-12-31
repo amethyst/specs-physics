ccee4441d88fb0416110e0f54bde88967d2e895a
kel <distransient@protonmail.com>
2018-12-31 12:49:01 -0500

    Prevent physics stepper spirals

--- src/systems/physics_stepper.rs
+++ src/systems/physics_stepper.rs
@@ -5,19 +5,24 @@ use amethyst::ecs::{Read, System, WriteExpect};
 /// Simulates a step of the physics world.
 pub struct PhysicsStepperSystem {
     intended_timestep: f32,
+    max_timesteps: i32,
 }
 
 impl Default for PhysicsStepperSystem {
     fn default() -> Self {
         PhysicsStepperSystem {
             intended_timestep: 1.0 / 120.0,
+            max_timesteps: 10,
         }
     }
 }
 
 impl PhysicsStepperSystem {
-    pub fn new(intended_timestep: f32) -> Self {
-        PhysicsStepperSystem { intended_timestep }
+    pub fn new(intended_timestep: f32, max_timesteps: i32) -> Self {
+        PhysicsStepperSystem {
+            intended_timestep,
+            max_timesteps,
+        }
     }
 }
 
@@ -25,7 +30,6 @@ impl<'a> System<'a> for PhysicsStepperSystem {
     type SystemData = (WriteExpect<'a, PhysicsWorld>, Read<'a, Time>);
 
     // Simulate world using the current time frame
-    // TODO: Bound timestep deltas
     fn run(&mut self, (mut physical_world, time): Self::SystemData) {
         if physical_world.timestep() != self.intended_timestep {
             warn!("Physics world timestep out of sync with intended timestep! Leave me alone!!!");
@@ -33,10 +37,12 @@ impl<'a> System<'a> for PhysicsStepperSystem {
         }
 
         let mut delta = time.delta_seconds();
+        let mut steps = 0;
 
-        while delta >= self.intended_timestep {
+        while steps <= self.max_timesteps && delta >= self.intended_timestep {
             physical_world.step();
             delta -= self.intended_timestep;
+            steps += 1;
         }
     }
 }
