8379453deade77a8e018259281e54e867d751350
Will Crichton <wcrichto@cs.stanford.edu>
2020-04-09 17:29:13 -0700

    Add toml dependencies, move code into amethyst.rs

--- src/bodies.rs
+++ src/bodies.rs
@@ -53,21 +53,6 @@ pub trait Position<N: RealField>:
     fn set_isometry(&mut self, isometry: &Isometry3<N>) -> &mut Self;
 }
 
-#[cfg(feature = "amethyst")]
-impl Position<f32> for amethyst_core::Transform {
-    fn isometry(&self) -> &Isometry3<f32> {
-        self.isometry()
-    }
-
-    fn isometry_mut(&mut self) -> &mut Isometry3<f32> {
-        self.isometry_mut()
-    }
-
-    fn set_isometry(&mut self, isometry: &Isometry3<f32>) -> &mut Self {
-        self.set_isometry(*isometry)
-    }
-}
-
 /// The `PhysicsBody` `Component` represents a `PhysicsWorld` `RigidBody` in
 /// Specs and contains all the data required for the synchronisation between
 /// both worlds.
